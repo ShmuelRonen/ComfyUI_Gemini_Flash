299f6434233ab475a6dbb74676b8e946d95137fc
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2024-07-20 14:00:17 +0300

    added support for proxy
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- Gemini_Flash_Node.py
+++ Gemini_Flash_Node.py
@@ -1,30 +1,47 @@
 import os
 import json
 import google.generativeai as genai
-from io import BytesIO
+from contextlib import contextmanager
 from PIL import Image
-import torch
 
-p = os.path.dirname(os.path.realpath(__file__))
 
-def get_gemini_api_key():
+CONFIG_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+def get_gemini_api_key() -> str:
     try:
-        config_path = os.path.join(p, 'config.json')
-        with open(config_path, 'r') as f:  
+        config_path = os.path.join(CONFIG_PATH, 'config.json')
+        with open(config_path, 'r') as f:
             config = json.load(f)
-        api_key = config["GEMINI_API_KEY"]
+        return config["GEMINI_API_KEY"]
     except:
         print("Error: API key is required")
         return ""
-    return api_key
 
-def save_gemini_api_key(api_key):
-    config_path = os.path.join(p, 'config.json')
+
+def save_gemini_api_key(api_key) -> None:
+    config_path = os.path.join(CONFIG_PATH, 'config.json')
     config = {"GEMINI_API_KEY": api_key}
     with open(config_path, 'w') as f:
         json.dump(config, f, indent=4)
 
-class Gemini_Flash:
+
+@contextmanager
+def temporary_env_var(key: str, new_value):
+    old_value = os.environ.get(key)  # Save the old value
+    if new_value:
+        os.environ[key] = new_value  # Assign the new value
+    try:
+        yield
+    finally:
+        if new_value:  # Only if something was set
+            if old_value is not None:
+                os.environ[key] = old_value  # Restore the old value
+            else:
+                del os.environ[key]  # Remove temporary value
+
+
+class GeminiFlash:
 
     def __init__(self, api_key=None):
         self.api_key = api_key or get_gemini_api_key()
@@ -40,7 +57,8 @@ class Gemini_Flash:
                 "api_key": ("STRING", {"default": ""})  # Add api_key as an input
             },
             "optional": {
-                "image": ("IMAGE",),  
+                "image": ("IMAGE",),
+                "proxy": ("STRING", {"default": ""})
             }
         }
 
@@ -50,13 +68,16 @@ class Gemini_Flash:
 
     CATEGORY = "Gemini flash"
 
-    def tensor_to_image(self, tensor):
+    @staticmethod
+    def tensor_to_image(tensor):
         tensor = tensor.cpu()
         image_np = tensor.squeeze().mul(255).clamp(0, 255).byte().numpy()
         image = Image.fromarray(image_np, mode='RGB')
         return image
 
-    def generate_content(self, prompt, vision, api_key, image=None):
+    def generate_content(self, prompt, vision, api_key, image=None, proxy=None):
+        if not api_key and not self.api_key:
+            api_key = os.environ.get("GOOGLE_API_KEY")
         if api_key:
             self.api_key = api_key
             save_gemini_api_key(api_key)
@@ -67,23 +88,25 @@ class Gemini_Flash:
         model_name = 'gemini-1.5-flash'
         model = genai.GenerativeModel(model_name)
 
-        if not vision:
-            # Act like a text LLM
-            response = model.generate_content(prompt)
-            textoutput = response.text
-        else:
-            # Vision enabled
-            if image is None:
-                raise ValueError(f"{model_name} needs image")
+        with temporary_env_var("HTTPS_PROXY", proxy):
+            if not vision:
+                # Act like a text LLM
+                response = model.generate_content(prompt)
+                text_output = response.text
             else:
+                # Vision enabled
+                if image is None:
+                    raise ValueError(f"{model_name} needs image")
+
                 pil_image = self.tensor_to_image(image)
                 response = model.generate_content([prompt, pil_image])
-                textoutput = response.text
-        
-        return (textoutput,)
+                text_output = response.text
+
+        return (text_output,)
+
 
 NODE_CLASS_MAPPINGS = {
-    "Gemini_Flash": Gemini_Flash,
+    "Gemini_Flash": GeminiFlash,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
